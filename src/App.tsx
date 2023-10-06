0beac64e8ce995903a4f7b7410a76e99b2903e82
gustavo-atanazio <gus.atanazio@gmail.com>
2023-10-06 10:53:35 -0300

    Criando item, renderizando lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import Form from "components/Form";
+import { Item } from "types/Item";
+import List from 'components/List';
 
 function App() {
+	const [itens, setItens] = useState<Item[]>([]);
+
 	function createItem(name: string, quantity: string) {
-		return { name, quantity };
+		const item: Item = {
+			name: name,
+			quantity: Number(quantity),
+			checked: false,
+			id: crypto.randomUUID()
+		};
+
+		setItens(prevState => [...prevState, item]);
 	}
 
 	return (
 		<main>
 			<h1>Lista de compras</h1>
 			<Form createItem={createItem}/>
+			<List itens={itens}/>
 		</main>
 	);
 }
