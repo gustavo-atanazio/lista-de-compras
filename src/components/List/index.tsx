97de4970c933ad154c7fe25066f338941b4da97e
gustavo-atanazio <gus.atanazio@gmail.com>
2023-10-06 11:07:55 -0300

    Repassando removeItem

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -4,14 +4,16 @@ import Item from 'components/Item';
 
 interface ListProps {
     itens: typeItem[]
+    removeItem: (id: string) => void
 }
 
-function List({ itens }: ListProps) {
+function List({ itens, removeItem }: ListProps) {
     return (
         <ul className={styles.item_list}>
             {itens.map(item => (
                 <Item
                     {...item}
+                    removeItem={removeItem}
                     key={item.id}
                 />
             ))}
