445e4a3de587571609f2d05fbff1a3c2c790c9fd
gustavo-atanazio <gus.atanazio@gmail.com>
2024-01-29 12:23:55 -0300

    Animações nos itens da lista

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,14 +1,28 @@
 import { memo } from 'react';
+import { motion } from 'framer-motion';
 import { AiOutlineClose } from 'react-icons/ai';
+
 import { useItemsContext } from 'context/Items';
 import { Item as typeItem } from 'types/Item';
+
 import styles from './Item.module.css';
 
-function Item({ name, quantity, checked, id }: typeItem) {
+type ItemProps = typeItem & {
+    animationDelay: number
+};
+
+function Item({ name, quantity, checked, id, animationDelay }: ItemProps) {
     const { checkItem, removeItem } = useItemsContext();
 
     return (
-        <li className={styles.item}>
+        <motion.li
+            className={styles.item}
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0 }}
+            transition={{ delay: animationDelay / 10, duration: 0.2 }}
+            layout
+        >
             <div className={styles.item__info}>
                 <span>{name}</span>
                 <span>{quantity}</span>
@@ -28,7 +42,7 @@ function Item({ name, quantity, checked, id }: typeItem) {
                     cursor={'pointer'}
                 />
             </div>
-        </li>
+        </motion.li>
     );
 }
 
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,22 +1,23 @@
+import { AnimatePresence } from 'framer-motion';
+
 import Item from 'components/Item';
 import { Item as typeItem } from 'types/Item';
 import styles from './List.module.css';
 
-interface ListProps {
-    items: typeItem[]
-}
-
-function List({ items }: ListProps) {
+function List({ items }: { items: typeItem[] }) {
     const hasChecked = items.some(item => item.checked);
 
     return (
         <ul className={`${styles.item_list} ${hasChecked ? styles.purchased_list : ''}`}>
-            {items.map(item => (
-                <Item
-                    {...item}
-                    key={item.id}
-                />
-            ))}
+            <AnimatePresence>
+                {items.map((item, index) => (
+                    <Item
+                        {...item}
+                        animationDelay={index}
+                        key={item.id}
+                    />
+                ))}
+            </AnimatePresence>
         </ul>
     );
 }
